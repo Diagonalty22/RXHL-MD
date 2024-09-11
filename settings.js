5c5cdf6c78ac2b7c75aad46c88a2ede8e038b437
Diagonalty22 <exdickzpb@gmail.com>
2024-09-11 15:35:50 +0700

    upload via termux

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6285601800364']
+global.botname = 'ིིིིིིོོུུུུུུ 𝗬𝗮𝗻𝘇奈メ𝐕𝐚𝐧𝐳🕊∞'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "🕊∞ᴹᴿメ 𝐇𝐞𝐧𝐝𝐫𝐚 [𝗝𝗕]"
+global.sticker2 = "🌜𝐖𝐄𝐋𝐋......"
